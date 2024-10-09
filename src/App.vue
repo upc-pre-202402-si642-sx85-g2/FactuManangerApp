80e36567eec4913a7c6c680c82dc2a4675a63114
Samira-AA <samirajetzabel@gmail.com>
2024-10-09 17:22:41 -0500

    chore: initial configuration

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <span class="peña">
+    <img src="./assets/peña.png" alt="Manuel Peña">
+  </span>
+
+</template>
+
+<style>
+.peña{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
