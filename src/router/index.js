e7c89bf83d890085efbbd05ca94ea4c3ebb499ab
Miguel Angel Ybañez Esquerre <arcangel080902@gmail.com>
2024-10-10 12:57:58 -0500

    feat: domain context

--- src/router/index.js
+++ src/router/index.js
@@ -7,10 +7,11 @@ const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/login',    component: TheHomeContent},
+        { path: '/',        redirect: '/login'},
         { path: '/register', component: ExaminerPerformanceOverview},
-        { path: '/forget-password',        redirect: '/home'},
-        { path: '/home',        redirect: '/home'},
-        { path: '/forget-password',        redirect: '/home'},
+        //{ path: '/forget-password',        },
+        //{ path: '/home',        },
+        //{ path: '/historial',        },
         { path: '/:pathMatch(.*)', redirect: '/404' },
         { path: '/404', name: '404', component: PageNotFound }
     ]
