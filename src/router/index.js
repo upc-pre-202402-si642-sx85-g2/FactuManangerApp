f6581fad9a0ad90db8d6eed312aa89c7b305c7d2
Miguel Angel Ybañez Esquerre <arcangel080902@gmail.com>
2024-10-10 13:27:50 -0500

    fix: router

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,17 @@
 import {createRouter, createWebHistory} from "vue-router";
-import TheHomeContent from "@/public/pages/the-home-content.component.vue";
-import PageNotFound from "@/public/pages/page-not-found.component.vue";
-import ExaminerPerformanceOverview from "@/nursing/pages/examiner-performance-overview.component.vue";
+
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/login',    component: TheHomeContent},
-        { path: '/',        redirect: '/login'},
-        { path: '/register', component: ExaminerPerformanceOverview},
+        //{ path: '/login',    component: },
+        //{ path: '/',        redirect: '/login'},
+        //{ path: '/register', component: },
         //{ path: '/forget-password',        },
         //{ path: '/home',        },
         //{ path: '/historial',        },
         { path: '/:pathMatch(.*)', redirect: '/404' },
-        { path: '/404', name: '404', component: PageNotFound }
+        //{ path: '/404', name: '404', component:  }
     ]
 });
 
