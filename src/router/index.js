cbcad8e5a4a3dc1ae1b0089d9f11be44b08167aa
U202210138 <129425974+U202210138@users.noreply.github.com>
2024-10-13 14:40:13 -0500

    feat: added dependencies and path home

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import {createRouter, createWebHistory} from "vue-router";
 import LoginRegisterAndForgetPassword from "../domain/pages/login-register-and-forget-password.component.vue";
 import portfolioComponent from "../portfolio/pages/portfolio.component.vue";
+import homeContentComponent from "../home/home-content/home-content.component.vue";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -11,7 +12,7 @@ const router = createRouter({
         { path: '/register', component: LoginRegisterAndForgetPassword},
         { path: '/forget-password', component: LoginRegisterAndForgetPassword},
         {path: '/portfolio', component: portfolioComponent},
-        //{ path: '/home',        },
+        { path: '/home', component:homeContentComponent},
         //{ path: '/historial',        },
         { path: '/:pathMatch(.*)', redirect: '/404' },
         //{ path: '/404', name: '404', component:  }
