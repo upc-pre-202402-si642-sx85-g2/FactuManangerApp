111d94829bb8e12ef5bc0a943da4cc05960971b8
U202210138 <129425974+U202210138@users.noreply.github.com>
2024-10-13 14:39:48 -0500

    feat: added home-content.component.vue

---
+++ src/home/home-content/home-content.component.vue
@@ -0,0 +1,67 @@
+<script>
+import HomeCardContent from "../home-card/home-card-content.component.vue";
+import sidebar from "../../public/sidebar.component.vue";
+
+export default {
+  name: "home-content",
+  components: {sidebar, HomeCardContent}
+}
+</script>
+
+<template>
+  <div class="container">
+    <!--<sidebar></sidebar> cambiale de nombre oe-->
+    <sidebar></sidebar>
+    <home-card-content></home-card-content>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+}
+
+@media (max-width: 767px) {
+  .container {
+    flex-direction: column;
+  }
+
+  .sidebar {
+    display: none;
+  }
+
+  .toggle-sidebar {
+    display: block;
+    margin: 10px;
+  }
+}
+@media (max-width: 1026px) {
+  .container {
+    flex-direction: column;
+  }
+
+  .sidebar {
+    display: none;
+  }
+
+  .toggle-sidebar {
+    display: block;
+    margin: 10px;
+  }
+}
+
+@media (min-width: 768px) {
+  .toggle-sidebar {
+    display: none;
+  }
+}
+@media (min-width: 1026px) {
+  .toggle-sidebar {
+    display: none;
+  }
+}
+home-card-content {
+  margin-left: auto;
+}
+
+</style>
