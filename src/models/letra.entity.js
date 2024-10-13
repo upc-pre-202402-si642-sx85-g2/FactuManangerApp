4095584437a5f1c9fd952d6ccceff064c4f74d1f
U202210138 <129425974+U202210138@users.noreply.github.com>
2024-10-13 14:39:18 -0500

    feat: added letra entity

---
+++ src/models/letra.entity.js
@@ -0,0 +1,19 @@
+export class letra {
+    constructor(bank="", faceValue=0, tea=0, tcea=0, period=0, tep=0, discountRate=0, deliveredValue=0, receivedValue=0, number=0, ruc="", emissionDate="", expirationDate="", discountDate="", sold=false){
+        this.bank = bank;
+        this.faceValue = faceValue;
+        this.tea = tea;
+        this.tcea = tcea;
+        this.period = period;
+        this.tep = tep;
+        this.discountRate = discountRate;
+        this.deliveredValue = deliveredValue;
+        this.receivedValue = receivedValue;
+        this.number = number;
+        this.ruc = ruc;
+        this.emissionDate = emissionDate;
+        this.expirationDate = expirationDate;
+        this.discountDate = discountDate;
+        this.sold = sold;
+    }
+}
