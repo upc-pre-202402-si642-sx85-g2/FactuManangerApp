39434e725a6c024ec30a3537527e74bc0aa09450
Samira-AA <samirajetzabel@gmail.com>
2024-10-30 17:22:24 -0500

    feat: add routing paths

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,19 @@
 import {createRouter, createWebHistory} from "vue-router";
 import LoginRegisterAndForgetPassword from "../domain/pages/login-register-and-forget-password.component.vue";
-import portfolioComponent from "../portfolio/pages/portfolio.component.vue";
-import modalComponent from "../operations/components/modal.component.vue";
-import operationsComponent from "../operations/components/operations.component.vue";
+import operationsPage from "../operations/pages/operations.page.vue";
+import portfolioPage from "../portfolio/pages/portfolio.page.vue";
+
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
 
-        { path: '/login', component: operationsComponent},
+        { path: '/login', component: LoginRegisterAndForgetPassword},
         { path: '/', redirect: '/login'},
         { path: '/register', component: LoginRegisterAndForgetPassword},
         { path: '/forget-password', component: LoginRegisterAndForgetPassword},
-        {path: '/portfolio', component: portfolioComponent},
+        {path: '/portfolio', component: portfolioPage},
+        {path: '/operations', component: operationsPage},
         //{ path: '/home',        },
         //{ path: '/historial',        },
         { path: '/:pathMatch(.*)', redirect: '/404' },
