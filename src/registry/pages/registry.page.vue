d8b36ddc423c076fdc2e423208f87a2951ccb5d3
Miguel Angel Ybañez Esquerre <109547479+Miguel080902@users.noreply.github.com>
2024-10-30 21:07:45 -0500

    feat: empty registry

--- src/registry/pages/registry.page.vue
+++ src/registry/pages/registry.page.vue
@@ -1,46 +1,14 @@
 <script>
-import emptyPortfolio from "../../portfolio/components/empty-portfolio.component.vue";
+import EmptyRegistry from "../components/empty-registry.component.vue";
 import RegistryTable from "../components/registry-table.component.vue";
 
 export default {
   name: "registry",
-  components: { RegistryTable, emptyPortfolio },
+  components: { RegistryTable, EmptyRegistry },
   data() {
     return {
       letters: [
-        {
-          bank: "BCP",
-          nominalValue: 1000,
-          tea: 5.0,
-          tcea: 5.5,
-          periodInDays: 30,
-          tep: 0.5,
-          discountedRate: 4.5,
-          deliveredValue: 950,
-          receivedValue: 1000
-        },
-        {
-          bank: "Interbank",
-          nominalValue: 2000,
-          tea: 6.0,
-          tcea: 6.5,
-          periodInDays: 60,
-          tep: 0.6,
-          discountedRate: 5.5,
-          deliveredValue: 1900,
-          receivedValue: 2000
-        },
-        {
-          bank: "Scotiabank",
-          nominalValue: 3000,
-          tea: 7.0,
-          tcea: 7.5,
-          periodInDays: 90,
-          tep: 0.7,
-          discountedRate: 6.5,
-          deliveredValue: 2850,
-          receivedValue: 3000
-        }
+
       ]
     };
   }
@@ -51,7 +19,7 @@ export default {
   <div class="registry-container">
     <div class="content">
       <h1 style="text-align: center">Registro de Venta de Letra</h1>
-      <empty-portfolio v-if="letters.length === 0" />
+      <empty-registry v-if="letters.length === 0" />
       <registry-table v-else :letters="letters" />
     </div>
   </div>
