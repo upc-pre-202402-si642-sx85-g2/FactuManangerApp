0124fd2e50476efbbc2054a9bd285339278ea826
Manuel Peña <u202210138@upc.edu.pe>
2024-11-02 01:13:35 -0500

    feat: created user entity

---
+++ src/models/user.entity.js
@@ -0,0 +1,9 @@
+export class User {
+    constructor(nombre_completo, dni, email, password, fecha_nacimiento) {
+        this.nombre_completo = nombre_completo;
+        this.dni = dni;
+        this.email = email;
+        this.password = password;
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+}
