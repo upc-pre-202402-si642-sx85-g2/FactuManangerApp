6e69e2a492a72e3ac6c8069db91002cc05a88e91
Manuel Peña <u202210138@upc.edu.pe>
2024-11-02 01:13:52 -0500

    feat: added home connection with the backend

--- src/home/home-card/home-card-content.component.vue
+++ src/home/home-card/home-card-content.component.vue
@@ -1,6 +1,6 @@
 <script>
-import { letra } from "../../models/letra.entity.js";
 import { LetraService } from "../../services/letra.service.js";
+import { Letra } from "../../models/letra.entity.js";
 
 export default {
   name: "home-card-content",
@@ -9,43 +9,59 @@ export default {
       letras: [],
       letrasApiService: new LetraService(),
       lastLetra: null,
-      lastSoldLetra: null
+      cantidadLetras: 0,
+      valorNominalTotal: 0
     };
   },
   async created() {
-    const response = await this.letrasApiService.getLetra();
-    this.letras = response.data.map(item => new letra(
-        item.Bank,
-        item.FaceValue,
-        item.TEA,
-        item.TCEA,
-        item.Period,
-        item.TEP,
-        item.DiscountRate,
-        item.DeliveredValue,
-        item.ReceivedValue,
-        item.Number,
-        item.RUC,
-        item.EmissionDate,
-        item.ExpirationDate,
-        item.DiscountDate,
-        item.Sold
-    ));
-    const soldLetra = this.letras.filter(letra => letra.sold === true);
-    if (soldLetra.length > 0) {
-      this.lastSoldLetra = soldLetra[soldLetra.length - 1];
-    }
-    if (this.letras.length > 0) {
-      this.lastLetra = this.letras[this.letras.length - 1];
+    try {
+      const userId = sessionStorage.getItem('userId');
+      if (!userId) {
+        throw new Error('User ID not found in local storage');
+      }
+
+      // Fetch cartera
+      const carteraResponse = await this.letrasApiService.getCarteraByUserId(userId);
+      const cartera = carteraResponse.data[0];
+      this.cantidadLetras = cartera.cantidad_letras;
+      this.valorNominalTotal = cartera.valor_nominal_total;
+
+      // Fetch letras
+      const letrasResponse = await this.letrasApiService.getLetrasByCarteraId(cartera._id);
+      this.letras = letrasResponse.data.map((letra, index) => {
+        const formattedLetra = new Letra(
+            index + 1, // Set the letter number as its position
+            letra.carteraId,
+            letra.razon_social,
+            letra.ruc,
+            this.formatDate(letra.fecha_emision),
+            this.formatDate(letra.fecha_descuento),
+            this.formatDate(letra.fecha_vencimiento),
+            letra.valor_nominal,
+            this.formatDate(letra.createdAt),
+            this.formatDate(letra.updateAt)
+        );
+        return formattedLetra;
+      });
+      if (this.letras.length > 0) {
+        this.lastLetra = this.letras[this.letras.length - 1];
+      }
+
+    } catch (error) {
+      console.error('Error fetching data:', error);
     }
   },
   methods: {
     formatCurrency(value) {
       return new Intl.NumberFormat('es-PE', { style: 'currency', currency: 'PEN' }).format(value);
+    },
+    formatDate(date) {
+      return new Date(date).toISOString().split('T')[0]; // Format date to YYYY-MM-DD
     }
   }
 };
 </script>
+
 <template>
   <div class="container">
     <div class="title">
@@ -57,13 +73,13 @@ export default {
         <div class="card-item special-card">
           <h2 class="black-text no-wrap">Cantidad de Letras</h2>
           <div class="card">
-            <h2 class="black-text">{{ letras.length }}</h2>
+            <h2 class="black-text">{{ cantidadLetras }}</h2>
           </div>
         </div>
         <div class="card-item special-card">
           <h2 class="black-text no-wrap">Valor Nominal</h2>
           <div class="card">
-            <h2 class="black-text">{{ formatCurrency(letras.reduce((total, letra) => total + letra.faceValue, 0)) }}</h2>
+            <h2 class="black-text">{{ formatCurrency(valorNominalTotal) }}</h2>
           </div>
         </div>
       </div>
@@ -75,27 +91,27 @@ export default {
         <div class="card-content-grid">
           <div class="card-item">
             <h2 class="blue-text no-wrap">Nro. Letra</h2>
-            <h2 class="black-text">{{ lastLetra.number }}</h2>
+            <h2 class="black-text">{{ lastLetra._id }}</h2>
           </div>
           <div class="card-item">
             <h2 class="blue-text no-wrap">Razón social</h2>
-            <h2 class="black-text">{{ lastLetra.ruc }}</h2>
+            <h2 class="black-text">{{ lastLetra.razon_social }}</h2>
           </div>
           <div class="card-item">
             <h2 class="blue-text no-wrap">Fecha de emisión</h2>
-            <h2 class="black-text">{{ lastLetra.emissionDate }}</h2>
+            <h2 class="black-text">{{ lastLetra.fecha_emision }}</h2>
           </div>
           <div class="card-item">
             <h2 class="blue-text no-wrap">Fecha de vencimiento</h2>
-            <h2 class="black-text">{{ lastLetra.expirationDate }}</h2>
+            <h2 class="black-text">{{ lastLetra.fecha_vencimiento }}</h2>
           </div>
           <div class="card-item">
             <h2 class="blue-text no-wrap">Fecha de descuento</h2>
-            <h2 class="black-text">{{ lastLetra.discountDate }}</h2>
+            <h2 class="black-text">{{ lastLetra.fecha_descuento }}</h2>
           </div>
           <div class="card-item">
             <h2 class="blue-text no-wrap">Valor Nominal</h2>
-            <h2 class="black-text">{{ formatCurrency(lastLetra.faceValue) }}</h2>
+            <h2 class="black-text">{{ formatCurrency(lastLetra.valor_nominal) }}</h2>
           </div>
         </div>
       </div>
@@ -105,7 +121,6 @@ export default {
     </div>
   </div>
 </template>
-
 <style scoped>
 .container {
   width: 100%;
@@ -201,6 +216,7 @@ export default {
   .card-content-grid {
     grid-template-columns: repeat(4, 1fr); /* Ajustar a 4 columnas en pantallas medianas */
   }
+
   .container {
     margin: 0 320px; /* Ajustar el margen para dejar espacio para el sidebar */
   }
@@ -210,6 +226,7 @@ export default {
   .card-content-grid {
     grid-template-columns: repeat(3, 1fr); /* Ajustar a 3 columnas en pantallas medianas */
   }
+
   .container {
     margin: 0 320px; /* Ajustar el margen para dejar espacio para el sidebar */
   }
@@ -229,6 +246,7 @@ export default {
   .card-content-grid {
     grid-template-columns: repeat(2, 1fr); /* Ajustar a 2 columnas en pantallas pequeñas */
   }
+
   .container {
     margin: 0 auto;
   }
@@ -244,17 +262,14 @@ export default {
   .card-content-grid {
     grid-template-columns: 1fr; /* Ajustar a 1 columna en pantallas muy pequeñas */
   }
+
   .card-content-grid-sold {
     grid-template-columns: 1fr; /* Ajustar a 2 columnas en pantallas pequeñas */
     margin-left: auto;
   }
+
   .container {
     margin: 0 auto;
   }
 }
 </style>
-
-
-
-
-
