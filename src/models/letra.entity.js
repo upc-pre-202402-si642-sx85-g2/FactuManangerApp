1119b94f14f1fb29475385bea4661fef072bcc70
Manuel Peña <u202210138@upc.edu.pe>
2024-11-02 01:14:32 -0500

    feat: updated letra

--- src/models/letra.entity.js
+++ src/models/letra.entity.js
@@ -1,19 +1,14 @@
-export class letra {
-    constructor(bank="", faceValue=0, tea=0, tcea=0, period=0, tep=0, discountRate=0, deliveredValue=0, receivedValue=0, number=0, ruc="", emissionDate="", expirationDate="", discountDate="", sold=false){
-        this.bank = bank;
-        this.faceValue = faceValue;
-        this.tea = tea;
-        this.tcea = tcea;
-        this.period = period;
-        this.tep = tep;
-        this.discountRate = discountRate;
-        this.deliveredValue = deliveredValue;
-        this.receivedValue = receivedValue;
-        this.number = number;
+export class Letra {
+    constructor(_id, carteraId, razon_social, ruc, fecha_emision, fecha_descuento, fecha_vencimiento, valor_nominal, createdAt, updateAt) {
+        this._id = _id;
+        this.carteraId = carteraId;
+        this.razon_social = razon_social;
         this.ruc = ruc;
-        this.emissionDate = emissionDate;
-        this.expirationDate = expirationDate;
-        this.discountDate = discountDate;
-        this.sold = sold;
+        this.fecha_emision = fecha_emision;
+        this.fecha_descuento = fecha_descuento;
+        this.fecha_vencimiento = fecha_vencimiento;
+        this.valor_nominal = valor_nominal;
+        this.createdAt = createdAt;
+        this.updateAt = updateAt;
     }
 }
